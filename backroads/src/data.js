225f65345ad835975ce4efd70ddeaf744cdc4a31
sagar-ss <sagarsrivastava0302@gmail.com>
2023-10-10 09:57:25 +0530

    tour components converted

--- backroads/src/data.js
+++ backroads/src/data.js
@@ -1,3 +1,8 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+
 export const pageLinks = [
     {id:1, href:'#home', text:'home'},
     {id:2, href:'#about', text:'about'},
@@ -15,4 +20,52 @@ export const services = [
     {id:1, icon:'fas fa-wallet fa-fw', title:'saving money', text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'},
     {id:2, icon:'fas fa-tree fa-fw', title:'endless hiking', text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'},
     {id:3, icon:'fas fa-socks fa-fw', title:'amazing comfort', text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'}
+]
+
+export const tours = [
+    {
+        id:1,
+        image:tour1,
+        date:'august 26th, 2020',
+        title:'Tibet Adventure',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        icon:'fas fa-map',
+        location:'china',
+        days:6,
+        cost:2100
+    },
+    {
+        id:2,
+        image:tour2,
+        date:'august 26th, 2020',
+        title:'Tibet Adventure',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        icon:'fas fa-map',
+        location:'china',
+        days:10,
+        cost:3100
+    },
+    {
+        id:3,
+        image:tour3,
+        date:'august 26th, 2020',
+        title:'Tibet Adventure',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        icon:'fas fa-map',
+        location:'china',
+        days:3,
+        cost:1100
+    },
+    {
+        id:4,
+        image:tour4,
+        date:'august 26th, 2020',
+        title:'Tibet Adventure',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        icon:'fas fa-map',
+        location:'china',
+        days:20,
+        cost:21000
+    },
+
 ]
